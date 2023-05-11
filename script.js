84439251403bebe9599da880a0808fc376dc5a41
kunalkamdi50 <128037794+kunalkamdi50@users.noreply.github.com>
2023-05-11 16:16:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,49 +1,40 @@
 //your code here
 
 class OutOfRangeError extends Error {
-  constructor(value) {
-    super(`Expression should only consist of integers and +-/* characters and not ${value}`);
-    this.name = this.constructor.name;
-  }
+	constructor(params) {
+		super(params);
+		this.name = "OutOfRangeError";
+	}
 }
 
-class InvalidExprError extends Error {
-  constructor() {
-    super('Expression should not have an invalid combination of expression');
-    this.name = this.constructor.name;
-  }
-}
+class InvalidExprError extends Error{
+	constructor(params) {
+		super(params);
+		this.name = "InvalidExprError";
+	} 
+} 
 
-function evalString(expr) {
-  // Check for invalid characters
-  const regex = /^[0-9+\-*/\s]+$/;
-  if (!regex.test(expr)) {
-    throw new OutOfRangeError(expr);
-  }
+let btn = document.getElementById("btn");
 
-  // Check for invalid expression combinations
-  const invalidRegex = /[+\-*/]{2,}/;
-  if (invalidRegex.test(expr)) {
-    throw new InvalidExprError();
-  }
-
-  // Check for invalid starting or ending operators
-  if (/^[+\-*/]/.test(expr)) {
-    throw new SyntaxError('Expression should not start with invalid operator');
-  }
-  if (/[+\-*/]$/.test(expr)) {
-    throw new SyntaxError('Expression should not end with invalid operator');
-  }
-
-  // Evaluate expression
-  return eval(expr);
-}
-
-// Example usage
+btn.addEventListener('click', (e)=>{
+	let input = document.getElementById("input1").value;
+	let exprarray = input.split("");
 try {
-  const result = evalString('1 + 2 - 3 * 4 / 5');
-  console.log(result); // -0.19999999999999996
-} catch (e) {
-  console.error(e);
+	let op = ["+", "/", "*", "-"]
+	op.map(op=>{
+		if(exprarray[0] == op || exprarray[exprarray.length-1] == op)
+		throw new OutOfRangeError("Expression should not start with invalid operator");
+	})
+	
+	
+	if(exprarray.includes("a"))
+		throw new InvalidExprError("Expression should only consist of integers and +-/* characters and not ");
+	document.write(eval(input));
+	
+} catch (Error) {
+	document.write(Error.message);
 }
 
+
+	
+});
